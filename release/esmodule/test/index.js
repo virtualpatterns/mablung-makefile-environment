2990c719ef2488b8042b221373f8698031aabd9b
virtualpatterns.com <code@virtualpatterns.com>
2021-06-19 22:41:47 -0400

    changes to fix replaced/extended rules

--- release/esmodule/test/index.js
+++
@@ -1,4 +0,0 @@
-import { createRequire as _createRequire } from "module";const Require = _createRequire(import.meta.url);
-export const OK = Require.resolve('./index.test.js');
-
-//# sourceMappingURL=index.js.map
