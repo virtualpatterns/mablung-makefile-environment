f5469de8d5dffac1586e8b19755fb706e2a8a94c
virtualpatterns.com <code@virtualpatterns.com>
2021-07-01 21:44:01 -0400

    removing release from git

--- release/esmodule/library/path.js
+++
@@ -1,6 +0,0 @@
-import { createRequire as _createRequire } from "module";
-const Require = _createRequire(import.meta.url);
-
-export const Path = Require.resolve('../../../makefile');
-
-//# sourceMappingURL=path.js.map
--- release/resource/index.cjs
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const Require = require;
-module.exports = {
-  'OK': Require.resolve('./index.json')
-};
-
-//# sourceMappingURL=index.cjs.map
