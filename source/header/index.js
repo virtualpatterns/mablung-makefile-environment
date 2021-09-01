ce16ed8f47e95f5349fbdd471274b93025224163
virtualpatterns.com <code@virtualpatterns.com>
2021-09-01 10:59:10 -0400

    updates

--- source/header/index.js
+++ source/header/index.js
@@ -1,21 +1,11 @@
-import { Package } from '@virtualpatterns/mablung-makefile/package'
+import '@virtualpatterns/mablung-makefile/header'
 
 const Process = process
 
 Process.exitCode = 0
 
 try {
-
-  console.log()
-  console.log(`Package:     ${Package.name}`)
-  console.log(`Description: ${Package.description}`)
-  console.log(`Version:     ${Package.version}`)
-  console.log(`License:     ${Package.license}`)
-  console.log(`Author:      ${Package.author}`)
-  console.log(`Repository:  ${Package.repository.url}`)
-  console.log(`Source:      ${Process.env.SOURCE_PATH}`)
   console.log(`Environment: ${Process.env.NODE_ENV}`)
-
 /* c8 ignore next 4 */
 } catch (error) {
   console.error(error)
