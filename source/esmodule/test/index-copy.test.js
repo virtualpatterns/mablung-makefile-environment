560cd3db83851d4494bf337607218eb2fa40e627
virtualpatterns.com <code@virtualpatterns.com>
2022-02-02 21:58:05 -0500

    updates

--- source/esmodule/test/index-copy.test.js
+++ source/esmodule/test/index-copy.test.js
@@ -4,22 +4,14 @@ import Test from 'ava'
 
 const FolderPath = __folderPath
 
-Test('./resource/copy/makefile', async (test) => {
-  test.false(await FileSystem.pathExists(Path.resolve(FolderPath, test.title)))
-})
-
-Test('./resource/copy/index.js', async (test) => {
-  test.true((await import(test.title)).OK)
+Test('./resource/copy/folder/index.json', async (test) => {
+  test.true((await FileSystem.readJson(Path.resolve(FolderPath, test.title), { 'encoding': 'utf-8' })).OK)
 })
 
 Test('./resource/copy/index.json', async (test) => {
   test.true((await FileSystem.readJson(Path.resolve(FolderPath, test.title), { 'encoding': 'utf-8' })).OK)
 })
 
-Test('./resource/copy/folder/index.js', async (test) => {
-  test.true((await import(test.title)).OK)
-})
-
-Test('./resource/copy/folder/index.json', async (test) => {
-  test.true((await FileSystem.readJson(Path.resolve(FolderPath, test.title), { 'encoding': 'utf-8' })).OK)
+Test('./resource/copy/makefile', async (test) => {
+  test.false(await FileSystem.pathExists(Path.resolve(FolderPath, test.title)))
 })
