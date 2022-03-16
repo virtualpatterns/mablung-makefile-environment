4bce9b823076dbf04d8ce9deb7b892b743c019dd
virtualpatterns.com <code@virtualpatterns.com>
2022-03-15 21:54:37 -0400

    updates

--- source/test/dependency.test.js
+++ source/test/dependency.test.js
@@ -17,3 +17,28 @@ Test('default', async (test) => {
   test.deepEqual(dependency.unused, [])
 
 })
+
+Test('section', async (test) => {
+
+  let dependency = await Check(Process.cwd(), {
+    'ignoreMatch': [
+      '@babel/cli',
+      '@babel/core',
+      '@babel/plugin-syntax-import-meta',
+      '@babel/preset-env',
+      '@virtualpatterns/babel-plugin-mablung-replace-identifier',
+      '@virtualpatterns/babel-plugin-mablung-replace-string-literal',
+      '@virtualpatterns/babel-preset-mablung-makefile',
+      '@virtualpatterns/eslint-config-mablung-makefile',
+      '@virtualpatterns/mablung-makefile'
+    ],
+    'ignorePattern': [
+      'source/esmodule/test/resource/copy',
+      'source/esmodule/test/resource/custom',
+      'source/esmodule/test/resource/ignore'
+    ]
+  })
+
+  test.deepEqual(dependency.section, {})
+
+})
