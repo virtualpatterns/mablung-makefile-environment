a49fae9655f1b22258f030016823d77f4345a533
virtualpatterns.com <code@virtualpatterns.com>
2022-04-13 20:58:00 -0400

    updates

--- source/test/command/index.test.js
+++ source/test/command/index.test.js
@@ -1,5 +1,5 @@
-import { CreateLoggedProcess } from '@virtualpatterns/mablung-worker/test'
-import { ForkedProcess } from '@virtualpatterns/mablung-worker'
+import { CreateRandomId } from '@virtualpatterns/mablung-worker'
+import { LoggedForkedProcess } from '@virtualpatterns/mablung-worker/test'
 import FileSystem from 'fs-extra'
 import Path from 'path'
 import Test from 'ava'
@@ -9,35 +9,42 @@ const FolderPath = __folderPath
 const Process = process
 
 const DataPath = FilePath.replace('/release/', '/data/').replace('.test.js', '')
-const LogPath = DataPath.concat('.log')
-const LoggedProcess = CreateLoggedProcess(ForkedProcess, LogPath)
 
 Test.before(async () => {
-  await FileSystem.ensureDir(Path.dirname(LogPath))
-  return FileSystem.remove(LogPath)
+  await FileSystem.remove(DataPath)
+  return FileSystem.ensureDir(DataPath)
+})
+
+Test.beforeEach(async (test) => {
+
+  let id = await CreateRandomId()
+  let logPath = Path.resolve(DataPath, `${id}.log`)
+
+  test.context.logPath = logPath
+
 })
 
 Test('default', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'))
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'))
   test.is(await process.whenExit(), 1)
 })
 
 Test('get-version', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'), { 'get-version': true })
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'), { 'get-version': true })
   test.is(await process.whenExit(), 0)
 })
 
 Test('get-version throws Error', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'), { 'get-version': true }, { 'execArgv': [ ...Process.execArgv, '--require', Path.resolve(FolderPath, 'require/get-version.cjs') ] })
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'), { 'get-version': true }, { 'execArgv': [ ...Process.execArgv, '--require', Path.resolve(FolderPath, 'require/get-version.cjs') ] })
   test.is(await process.whenExit(), 1)
 })
 
 Test('get-path', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'), { 'get-path': true })
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'), { 'get-path': true })
   test.is(await process.whenExit(), 0)
 })
 
 Test('get-path throws Error', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'), { 'get-path': true }, { 'execArgv': [ ...Process.execArgv, '--require', Path.resolve(FolderPath, 'require/get-path.cjs') ] })
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'), { 'get-path': true }, { 'execArgv': [ ...Process.execArgv, '--require', Path.resolve(FolderPath, 'require/get-path.cjs') ] })
   test.is(await process.whenExit(), 1)
 })
--- source/test/make.test.js
+++ source/test/make.test.js
@@ -1,5 +1,5 @@
-import { CreateLoggedProcess } from '@virtualpatterns/mablung-worker/test'
-import { SpawnedProcess } from '@virtualpatterns/mablung-worker'
+import { CreateRandomId } from '@virtualpatterns/mablung-worker'
+import { LoggedSpawnedProcess } from '@virtualpatterns/mablung-worker/test'
 import FileSystem from 'fs-extra'
 import Path from 'path'
 import Test from 'ava'
@@ -8,25 +8,32 @@ const FilePath = __filePath
 const Process = process
 
 const DataPath = FilePath.replace('/release/', '/data/').replace('.test.js', '')
-const LogPath = DataPath.concat('.log')
-const LoggedProcess = CreateLoggedProcess(SpawnedProcess, LogPath)
 
 Test.before(async () => {
-  await FileSystem.ensureDir(Path.dirname(LogPath))
-  return FileSystem.remove(LogPath)
+  await FileSystem.remove(DataPath)
+  return FileSystem.ensureDir(DataPath)
+})
+
+Test.beforeEach(async (test) => {
+
+  let id = await CreateRandomId()
+  let logPath = Path.resolve(DataPath, `${id}.log`)
+
+  test.context.logPath = logPath
+
 })
 
 Test('default', async (test) => {
-  let process = new LoggedProcess(Process.env.MAKE_PATH, [])
+  let process = new LoggedSpawnedProcess(test.context.logPath, Process.env.MAKE_PATH, [])
   test.is(await process.whenExit(), 0)
 })
 
 Test('version', async (test) => {
-  let process = new LoggedProcess(Process.env.MAKE_PATH, [ '--dry-run', 'version' ])
+  let process = new LoggedSpawnedProcess(test.context.logPath, Process.env.MAKE_PATH, [ '--dry-run', 'version' ])
   test.is(await process.whenExit(), 0)
 })
 
 Test('build', async (test) => {
-  let process = new LoggedProcess(Process.env.MAKE_PATH, [ '--dry-run', 'build' ])
+  let process = new LoggedSpawnedProcess(test.context.logPath, Process.env.MAKE_PATH, [ '--dry-run', 'build' ])
   test.is(await process.whenExit(), 0)
 })
